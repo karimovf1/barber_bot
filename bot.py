2459fe1023006d3a6ed87b4f95400e1663983ed0
karimovf1 <akromkarimov271@gmail.com>
2025-07-14 21:01:13 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,28 +1,18 @@
-from telegram import Update, ReplyKeyboardMarkup
+from telegram import Update, BotCommand
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# Yangi tokening
-BOT_TOKEN = "7683754796:AAFsefo83v8AKsnB9cu1_eB-HB7LLoe83gs"
-
-# Foydalanuvchi uchun asosiy menyu
-def get_main_menu():
-    return ReplyKeyboardMarkup(
-        [
-            ["👤 Shaxsiy kabinet", "✂️ Xizmatlar"],
-            ["📅 Bandlovni bekor qilish", "🎁 Cashback"]
-        ],
-        resize_keyboard=True
-    )
+TOKEN = "7683754796:AAFsefo83v8AKsnB9cu1_eB-HB7LLoe83gs"
 
 # /start komandasi
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(
-        "Assalomu alaykum! 'Barber Shaxzod' botiga xush kelibsiz.\nBuyruqlardan birini tanlang 👇",
-        reply_markup=get_main_menu()
+        "Assalomu alaykum!\nXush kelibsiz! 👋\nBu test bosqichidagi bot."
     )
 
-# Asosiy ishga tushirish qismi
+# Botni ishga tushirish
 if __name__ == "__main__":
     app = ApplicationBuilder().token("7683754796:AAFsefo83v8AKsnB9cu1_eB-HB7LLoe83gs").build()
+
     app.add_handler(CommandHandler("start", start))
+
     app.run_polling()
